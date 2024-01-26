784e874f9107147882c9b6d66ba53e38daf1d749
Noor El-Din Walid <noorwalid72@yahoo.com>
2024-01-26 15:53:49 +0200

    Initial Commit

---
+++ UnitConverter-SwiftUI/UnitConverter_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverter_SwiftUIApp.swift
+//  UnitConverter-SwiftUI
+//
+//  Created by Noor Walid on 26/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverter_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
